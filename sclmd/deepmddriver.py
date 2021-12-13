97b92cde18d16f83f7963c02d45fc135162f797b
Aperture <6523942+xmhio@users.noreply.github.com>
2021-12-13 19:48:27 +0800

    Update

--- sclmd/deepmddriver.py
+++ sclmd/deepmddriver.py
@@ -1,106 +1,61 @@
-from lammps import lammps
-from sclmd.tools import get_atomname
-import sys
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
 
+import dpdata
 import numpy as np
 from deepmd.infer import DeepPot
 
+from sclmd.tools import get_atommass
+
 
 class deepmddriver():
-    def __init__(self, pbinfile, lmpinfile, cell=np.diag(10 * np.ones(3)).reshape([1, -1]),
-                 atype=[1, 0, 1], label="",
-                 constraints=[], tdir="./", lunit="Ang", eunit="eV", md2ang=0.06466,
-                 ):
-        self.pbinfile = pbinfile
-        self.lmpinfile = lmpinfile
-        self.cell = cell
-        self.atype = atype
-        self.md2ang = md2ang
-        self.constraints = constraints
-        self.label = label
-        self.lunit = lunit
-        self.eunit = eunit
-        if self.eunit == "eV":
-            self.para = 1.0
-        elif self.eunit == "Kcal/mole":
-            self.para = 0.04336344201657351
-        else:
-            print("Wrong vaule in eunit")
-            sys.exit(0)
-        self.runlmp()
-        self.dpstart()
+    def __init__(self, strinfile, fmt, pbinfile, md2ang=0.06466):
+        self.deepmdstr(strinfile, fmt, md2ang)
+        self.dpstart(pbinfile)
 
-    def runlmp(self):
-        self.lmp = structure(self.lmpinfile)
-        self.lmp.quit()
+    def deepmdstr(self, strinfile, fmt, md2ang):
+        str = dpdata.LabeledSystem(strinfile, fmt)
+        #str.to('lammps/lmp', 'data.lmp', frame_idx=0)
+        #str.to('vasp/poscar', 'POSCAR.vasp', frame_idx=0)
+        self.number = sum(str['atom_types'])
+        self.type = str['atom_types']
+        atomname = str['atom_names']
+        mass = [get_atommass(el) for el in atomname]
+        self.els = []
+        atomnamelist = []
+        for type in self.type:
+            self.els.append(mass[type])
+            atomnamelist.append(atomname[type])
+        self.xyz = str['coords'].flatten()
+        self.conv = md2ang*np.array([3*[1.0/np.sqrt(mass)]
+                                     for mass in self.els]).flatten()
+        self.axyz = []
+        for i, a in enumerate(self.els):
+            self.axyz.append([atomnamelist[i], self.xyz[i*3],
+                              self.xyz[i*3+1], self.xyz[i*3+2]])
+        self.cell = str['cells']
+        print("structure imported")
 
-    def dpstart(self):
-        self.dp = DeepPot(self.pbinfile)
+    def dpstart(self, pbinfile):
+        self.dp = DeepPot(pbinfile)
         print("deepmdkit launched")
         # print(self.conv.shape)
         self.initforce()
 
     def newx(self, q):
-        newxyz = self.lmp.xyz + self.lmp.conv*q
-        return newxyz.ctypes
+        return self.xyz + self.conv*q
 
     def absforce(self, q):
-        self.e, self.f, self.v = self.dp.eval(q, self.cell, self.atype)
-        return self.para*self.lmp.conv*np.array(self.f)
+        self.e, self.f, self.v = self.dp.eval(
+            self.newx(q), self.cell, self.type)
+        return self.conv*np.array(self.f)
 
     def initforce(self):
         print("Calculate zero displacement force")
-        self.f0 = self.absforce(np.zeros(3*self.lmp.number))
+        self.f0 = self.absforce(np.zeros(3*self.number))
 
     def force(self, q):
         return self.absforce(q) - self.f0
 
     def energy(self):
         return self.e
-
-
-class structure():
-    def __init__(self, lmpinfile, label="", constraints=[],
-                 tdir="./", lunit="Ang", eunit="eV", md2ang=0.06466,
-                 name="", cmdargs="-screen none".split(), ptr=None, comm=None
-                 ):
-        lammps.__init__(self, name=name, cmdargs=cmdargs, ptr=None, comm=None)
-        self.infile = lmpinfile
-        self.md2ang = md2ang
-        self.constraints = constraints
-        self.label = label
-        self.lunit = lunit
-        self.eunit = eunit
-        if self.eunit == "eV":
-            self.para = 1.0
-        elif self.eunit == "Kcal/mole":
-            self.para = 0.04336344201657351
-        else:
-            print("Wrong vaule in eunit")
-            sys.exit(0)
-        # start lammps
-        self.start()
-
-    def start(self):
-        print("LAMMPS launched")
-        #for line in lines: self.command(line)
-        self.type = np.array(self.gather_atoms("type", 0, 1))
-        #self.mass = np.array(self.gather_atoms("mass",1,1))
-        self.mass = self.extract_atom("mass", 2)
-        self.number = self.get_natoms()
-        self.els = []
-        for type in self.type:
-            self.els.append(self.mass[type])
-        self.xyz = self.gather_atoms("x", 1, 3)
-        self.conv = self.md2ang*np.array([3*[1.0/np.sqrt(mass)]
-                                          for mass in self.els]).flatten()
-        self.type = np.array(self.gather_atoms("type", 0, 1))
-        #self.mass = np.array(self.gather_atoms("mass",1,1))
-        self.axyz = []
-        for i, a in enumerate(self.els):
-            self.axyz.append([get_atomname(a), self.xyz[i*3],
-                              self.xyz[i*3+1], self.xyz[i*3+2]])
-
-    def quit(self):
-        self.close()
-        print("LAMMPS closed.")
