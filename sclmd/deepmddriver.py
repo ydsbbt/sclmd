17a380565254271b527811cc1ff4e7e47c005ece
Aperture <6523942+xmhio@users.noreply.github.com>
2021-12-17 21:49:51 +0800

    import System structures

--- sclmd/deepmddriver.py
+++ sclmd/deepmddriver.py
@@ -9,17 +9,23 @@ from sclmd.tools import get_atommass
 
 
 class deepmddriver():
-    def __init__(self, strinfile, fmt, pbinfile, md2ang=0.06466):
-        self.deepmdstr(strinfile, fmt, md2ang)
+    def __init__(self, strinfile, fmt, pbinfile, label, atomname=None, md2ang=0.06466):
+        self.deepmdstr(strinfile, fmt, label, atomname, md2ang)
         self.dpstart(pbinfile)
 
-    def deepmdstr(self, strinfile, fmt, md2ang):
-        str = dpdata.LabeledSystem(strinfile, fmt)
-        #str.to('lammps/lmp', 'data.lmp', frame_idx=0)
-        #str.to('vasp/poscar', 'POSCAR.vasp', frame_idx=0)
+    def deepmdstr(self, strinfile, fmt, label, atomname, md2ang):
+        if label == 'System':
+            str = dpdata.System(strinfile, fmt)
+            atomname = atomname
+        elif label == 'LabeledSystem':
+            str = dpdata.LabeledSystem(strinfile, fmt)
+            atomname = str['atom_names']
+        else:
+            raise ValueError("Label must be 'System' or 'LabeledSystem'")
+        str.to('lammps/lmp', 'data.lmp', frame_idx=0)
+        str.to('vasp/poscar', 'POSCAR.vasp', frame_idx=0)
         self.number = len(str['atom_types'])
         self.type = str['atom_types']
-        atomname = str['atom_names']
         mass = [get_atommass(el) for el in atomname]
         self.els = []
         atomnamelist = []
@@ -34,11 +40,18 @@ class deepmddriver():
             self.axyz.append([atomnamelist[i], self.xyz[i*3],
                               self.xyz[i*3+1], self.xyz[i*3+2]])
         self.cell = str['cells']
-        print("structure imported")
+        print("Structure imported")
+        print("\n"+"System info:")
+        print("Atom names:", atomname)
+        print("Atom masses:", mass)
+        print("Cell:", self.cell)
+        print("Total:", self.number)
+        print("Atom types:", self.type)
+        print("Postions of atoms:", self.xyz)
 
     def dpstart(self, pbinfile):
         self.dp = DeepPot(pbinfile)
-        print("deepmdkit launched")
+        print("Deepmdkit launched")
         # print(self.conv.shape)
         self.initforce()
 
@@ -60,9 +73,20 @@ class deepmddriver():
     def energy(self):
         return self.e
 
+
 if __name__ == '__main__':
-    dp = deepmddriver('OUTCAR', 'vasp/OUTCAR', 'graph.pb')
-    print('Energy: ',dp.energy())
-    print('Structure: ',dp.axyz)
-    print('Force init: ',dp.f0)
-    print('Delta Force at q=0: ',dp.force(np.zeros(3*dp.number)))
+    import numpy as np
+    import time
+    from tqdm import tqdm
+    dp = deepmddriver('water.lmp', 'lammps/lmp',
+                      'graph-compress.pb', 'System', ['O', 'H'])
+    #dp = deepmddriver('OUTCAR', 'vasp/outcar','graph-compress.pb', 'LabeledSystem')
+    print('Energy: ', dp.energy())
+    print('Structure: ', dp.axyz)
+    print('Force init: ', dp.f0)
+    st = time.time()
+    tt = 1000
+    for i in tqdm(range(tt)):
+        dp.force(np.zeros(3*dp.number))
+    print('Total:', (time.time()-st), 's')
+    print('Average:', (time.time()-st)/tt, 's')
