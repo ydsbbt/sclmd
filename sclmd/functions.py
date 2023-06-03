dfcdb140a022785178afb607eecc561937ad0d49
phyjtlu <43269340+phyjtlu@users.noreply.github.com>
2023-06-03 14:38:54 +0800

    Update functions.py
    fixed a bug in flinterp

--- sclmd/functions.py
+++ sclmd/functions.py
@@ -128,7 +128,7 @@ def flinterp(x, xs, ys):
 
     # linear interpolation
     dd = xs[id]-x
-    if dd < 0:
+    if dd > 0:
         return ys[id]+dd/(xs[id]-xs[id-1])*(ys[id]-ys[id-1])
     else:
         return ys[id]+dd/(xs[id]-xs[id+1])*(ys[id]-ys[id+1])
